f59d0d16cac0d02a10810a28705a6526304c94f3
Aditya Singh Parihar <201037@kit.ac.in>
2023-09-10 23:55:39 +0530

    Adding Contact Component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import Contact from "./Component/ContactForm";
+
+
+const App = () => {
+  return (
+    <Contact/>
+  );
+};
+
+export default App;
+
